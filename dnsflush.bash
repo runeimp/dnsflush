08e1a7f63e9d8d0068f87979d32f8137f5bd2b54
Mark Gardner <mark.gardner@twobitcircus.com>
2018-04-14 09:11:41 -0700

    Added macOS High Sierra

--- dnsflush.bash
+++ dnsflush.bash
@@ -4,13 +4,17 @@
 #
 # @author RuneImp <runeimp@gmail.com>
 # @licenses http://opensource.org/licenses/MIT
-# @see http://osxdaily.com/2008/03/21/how-to-flush-your-dns-cache-in-mac-os-x/
+#
+# @see http://osxdaily.com/2017/12/18/reset-dns-cache-macos-high-sierra/
+# @see http://osxdaily.com/2017/03/08/clear-dns-cache-macos-sierra/
 # @see http://osxdaily.com/2015/11/16/howto-flush-dns-cache-os-x-elcap/
 # @see http://blog.chlaird.com/2015/06/os-x-1011-el-capitan-flush-dns.html
+# @see http://osxdaily.com/2008/03/21/how-to-flush-your-dns-cache-in-mac-os-x/
 #
 #####
 # ChangeLog
 # ---------
+# 2018-04-14  1.2.0      Added support for macOS High Sierra and updated for Sierra
 # 2017-07-07  1.1.0      Added support for macOS Sierra
 # 2016-??-??  1.0.0      Initial creation
 #
@@ -21,10 +25,24 @@
 #
 declare -r APP_CLI="dnsflush"
 declare -r APP_NAME="DNS Flush"
-declare -r APP_VERSION="1.1.0"
+declare -r APP_VERSION="1.2.0"
 declare -r APP_LABEL="$APP_NAME v$APP_VERSION"
 
-
+declare -a OS_LIST_10
+OS_LIST_10[0]='Cheetah'        # 10.0
+OS_LIST_10[1]='Puma'           # 10.1
+OS_LIST_10[2]='Jaguar'         # 10.2
+OS_LIST_10[3]='Panther'        # 10.3
+OS_LIST_10[4]='Tiger'          # 10.4
+OS_LIST_10[5]='Leopard'        # 10.5
+OS_LIST_10[6]='Snow Leopard'   # 10.6
+OS_LIST_10[7]='Lion'           # 10.7
+OS_LIST_10[8]='Mountain Lion'  # 10.8
+OS_LIST_10[9]='Mavricks'       # 10.9
+OS_LIST_10[10]='Yosemite'      # 10.10
+OS_LIST_10[11]='El Capitan'    # 10.11
+OS_LIST_10[12]='Sierra'        # 10.12
+OS_LIST_10[13]='High Sierra'   # 10.13
 declare -r OS_NAME=$(uname -s)
 declare -r OS_VERSION=$(uname -r)
 
@@ -92,12 +110,24 @@ case "$OS_NAME" in
 		;;
 esac
 
+
 case "$OS_NAME" in
 	Darwin)
 		if [[ $OS_VERSION_MAJOR -eq 10 ]]; then
 			case $OS_VERSION_MINOR in
-				9 | 1[12])
-					# Mavricks, El Capitan, Sierra
+				12)
+					# Sierra
+					if [[ $OS_VERSION_PATCH -lt 3 ]]; then
+						sudo killall -HUP mDNSResponder
+					else
+						sudo killall -HUP mDNSResponder
+						sudo killall mDNSResponderHelper
+						sudo dscacheutil -flushcache
+					fi
+					dns_flushed=0
+					;;
+				9 | 11)
+					# Mavricks, El Capitan
 					dscacheutil -flushcache
 					sudo killall -HUP mDNSResponder
 					dns_flushed=0
@@ -108,8 +138,8 @@ case "$OS_NAME" in
 					sudo discoveryutil udnsflushcaches
 					dns_flushed=0
 					;;
-				7 | 8)
-					# Lion & Mountain Lion
+				7 | 8 | 13)
+					# Lion, Mountain Lion, High Sierra
 					sudo killall -HUP mDNSResponder
 					dns_flushed=0
 					;;
@@ -130,7 +160,7 @@ case "$OS_NAME" in
 
 			if [[ $dns_flushed -eq 0 ]]; then
 				# say DNS cache flushed
-				echo "DNS cache flushed"
+				echo "DNS cache flushed for ${OS_LIST_10[$OS_VERSION_MINOR]}"
 			fi
 		fi
 		;;
